336b684617edad3221376ed611e8132501ebbdc3
Shimoji, Kensuke <k.zimoc@gmail.com>
2022-05-28 20:09:55 +0900

    add navigation to header.

--- web-front/src/app/shared/layout/header.component.ts
+++ web-front/src/app/shared/layout/header.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'gh-layout-header',
   templateUrl: 'header.component.html',
   styleUrls: ['header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(private readonly router: Router) {}
+
+  goToTopPage() {
+    this.router.navigateByUrl('/');
+  }
+}
