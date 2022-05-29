6ba9665c1e4673e7642a0a5dc965581cf1484627
Shimoji, Kensuke <k.zimoc@gmail.com>
2022-05-30 00:58:33 +0900

    convert to material ui.

--- web-front/src/app/modules/glossary/glossary.module.ts
+++ web-front/src/app/modules/glossary/glossary.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from '../shared/material.module';
 import { CreateGlossaryComponent } from './create/create-glossary.component';
 import { GlossaryRoutingModule } from './glossary-routing.module';
 import { GlossaryComponent } from './glossary.component';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, GlossaryRoutingModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, GlossaryRoutingModule, MaterialModule],
   declarations: [GlossaryComponent, CreateGlossaryComponent],
 })
 export class GlossaryModule {}
