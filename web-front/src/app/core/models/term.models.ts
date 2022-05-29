6d09261b3c4448da5e4292372f23087044262d90
Shimoji, Kensuke <k.zimoc@gmail.com>
2022-05-30 04:32:29 +0900

    add term add service.

---
+++ web-front/src/app/core/models/term.models.ts
@@ -0,0 +1,7 @@
+export interface Term {
+  id: string;
+  glossaryId: string;
+  name: string;
+  yomi: string;
+  description: string;
+}
---
+++ web-front/src/app/core/services/term.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+export interface AddTermInput {
+  name: string;
+  yomi: string;
+  description: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TermService {
+  add(input: AddTermInput): Observable<string> {
+    console.log(`add term = ${JSON.stringify(input)}`);
+    return of('term-id-123');
+  }
+}
